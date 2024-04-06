c0ff569ee175ca4c078ceafdc4fe3f44eea1bc71
Malachi Burke <malachi.burke@gmail.com>
2024-04-05 19:11:16 -0700

    Now using Kconfig for I2C settings

--- main/main.cpp
+++ main/main.cpp
@@ -8,14 +8,16 @@
 
 static const char* TAG = "MAIN";
 
-#define I2C_SCL_IO				22	//19               /*!< gpio number for I2C master clock */
-#define I2C_SDA_IO				21	//18               /*!< gpio number for I2C master data  */
-#define I2C_FREQ_HZ				400000           /*!< I2C master clock frequency */
+#define I2C_SCL_IO              CONFIG_BQ27441_I2C_SCL      /*!< gpio number for I2C master clock  */
+#define I2C_SDA_IO              CONFIG_BQ27441_I2C_SDA      /*!< gpio number for I2C master data  */
+#define I2C_FREQ_HZ             CONFIG_BQ27441_I2C_FREQ     /*!< I2C master clock frequency */
 #define I2C_PORT_NUM			I2C_NUM_0        /*!< I2C port number for master dev */
 #define I2C_TX_BUF_DISABLE  	0                /*!< I2C master do not need buffer */
 #define I2C_RX_BUF_DISABLE  	0                /*!< I2C master do not need buffer */
 #define ACK_CHECK_EN                       0x1              /*!< I2C master will check ack from slave*/
 #define ACK_CHECK_DIS                      0x0              /*!< I2C master will not check ack from slave */
+
+#define TIMEOUT_APB_TICKS 0x1f // 800000 == 10 ms
 
 #define BQ27441_I2C_ADDRESS 0x55
 
@@ -41,7 +43,12 @@ void app_main(void)
     ESP_LOGI(TAG,"MAIN ENTRY");
 
     i2c_master_init();
+#ifdef CONFIG_IDF_TARGET_ESP32
     i2c_set_timeout(I2C_PORT_NUM,0xFFFFF);
+#else
+    // Guidance from https://github.com/espressif/esp-idf/issues/11397
+    i2c_set_timeout(I2C_PORT_NUM, TIMEOUT_APB_TICKS);
+#endif
 
     //xTaskCreatePinnedToCore(task_fuel_gauge, "fuel_gauge", 2048, (void* ) 0, 20, NULL,1);
 
